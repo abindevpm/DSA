6d3bce2c7116fff93ed2dbbafc1f6ef49a3dccb9
abindevpm <abindevpm6@gmail.com>
2026-06-06 10:38:52 +0530

    append implemented

--- week1/linkedlist/list.js
+++ week1/linkedlist/list.js
@@ -5,39 +5,56 @@ class Node {
     }
 }
 
-class Linkedlist {
-    constructor(){
-        this.next = null
-        this.size = 0;
-    }
-    isEmpty(){
-        return this.size === 0
-    }
-    
-    prepend(value){
-        const node = new Node (value)
-        if(this.head === null){
-            this.head = node
-        }else{
-            node.next = this.head
-            this.head = node
-        }
-    }
+ class Linkedlist {
+     constructor(){
+         this.head = null
+         this.size = 0;
+     }
+     
+      prepend(value){
+          const node = new Node (value)
+          if(this.head === null){
+              this.head = node
+          }else{
+              node.next = this.head
+              this.head = node
+          }
+      }
+      append(value){
+          const node = new Node (value)
+           if(this.head === null){
+               this.head = node
+           }else{
+               let prev = this.head
+                while(prev.next){
+                    prev = prev.next
+                }
+                prev.next = node
+           }
+           
+      }
+      
      
       display(){
-            let curr = this.head
-            let s = ''
-            while(curr){
-                s+=curr.value+'->'
-                curr = curr.next
-            }
-            console.log(s,null)
-      }    
-    
-}
-
- const list = new Linkedlist()
- list.prepend(1)
- list.prepend(2)
- list.display()
-
+          let curr = this.head
+          let s = ''
+           while(curr){
+               s+=curr.value+'->'
+               curr = curr.next
+           }
+           console.log(s,null)
+      }
+     
+ }
+ 
+  const list = new Linkedlist()
+  
+  list.prepend(1)
+  list.prepend(2)
+  list.append(3)
+  list.append(4)
+  
+  
+  
+  
+  list.display()
