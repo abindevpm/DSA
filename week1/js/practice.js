2d1b53c1ca805a8e66844d0578e19138de0c350c
abindevpm <abindevpm6@gmail.com>
2026-07-01 13:15:32 +0530

    remove dup without extra array and inbuilt

--- week1/js/practice.js
+++ week1/js/practice.js
@@ -52,3 +52,25 @@ let a = ["hello", "world"]
  }
  
  console.log(removeOdd(a))
+
+
+ //  removing Duplicates without inbuit and extra array
+
+ function removeDup(arr){
+    
+     for(let i = 0;i<arr.length;i++){
+          for(let j = i+1;j<arr.length;j++){
+               if(arr[i] === arr[j]){
+                   for(let k = j;k<arr.length-1;k++){
+                        arr[k] = arr[k+1]
+                   }
+                   arr.length--
+                   j--
+               }
+          }
+     }
+     return arr
+    
+}
+
+console.log(removeDup([1,2,3,1,2,3]))
