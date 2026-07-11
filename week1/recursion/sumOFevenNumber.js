50d71401c1752fd622c2ec667c702561344eec2b
abindevpm <abindevpm6@gmail.com>
2026-07-11 12:37:56 +0530

    sum of even using recusrion

--- week1/recursion/sumOFevenNumber.js
+++ week1/recursion/sumOFevenNumber.js
@@ -1,11 +1,12 @@
-function sumofEven(arr,index=0){
+function sumEven(arr,index=0){
     if(index === arr.length){
-        return 0
+        return 0;
     }
     if(arr[index]%2==0){
-        return arr[index]+sumofEven(arr,index+1)
+        return arr[index]+sumEven(arr,index+1)
     }
-    return sumofEven(arr,index+1)
+    return sumEven(arr,index+1)
 }
 
-console.log(sumofEven([1,2,3,4,8,]))
+
+console.log(sumEven([1,2,3,4,5,6]))
