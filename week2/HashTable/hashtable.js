4d0aec7d30a80b7e138731274d9b3d9a24536978
abindevpm <abindevpm6@gmail.com>
2026-08-08 23:43:51 +0530

    hash table set implemented

---
+++ week2/HashTable/hashtable.js
@@ -0,0 +1,35 @@
+class HashTable {
+    constructor(size){
+        this.table = new Array(size)
+        this.size = size
+    }
+    
+    hash(key){
+        let hash = 0;
+        
+        for(let  i = 0;i<key.length;i++){
+            hash+=key.charCodeAt(i)
+        }
+        return hash%this.size
+        
+    }
+    
+    set(key,value){
+        let index = this.hash(key)
+        this.table[index] = [key,value]
+        
+    }
+    
+    
+    
+}
+
+const ht = new HashTable (10)
+
+ht.set("name",10)
+ht.set('name',22)
+
+console.log(ht.table)
+
+
+
