750fc0e172f4decb0cf10ad3c9dadb1501e33505
abindevpm <abindevpm6@gmail.com>
2026-09-02 15:46:34 +0530

    insert implemented in Trie

---
+++ week3/Trie/trie.js
@@ -0,0 +1,26 @@
+class TrieNode {
+    constructor(){
+        this.children = {};
+        this.isEnd = false
+    }
+}
+
+class Trie{
+    constructor(){
+        this.root = new TrieNode()
+    }
+
+    insert(word){
+        let node = this.root
+
+        for(let ch of word){
+            if(!node.children[ch]){
+                node.children[ch] = new TrieNode()
+            }
+            node = node.children[ch]
+        }
+        node.isEnd = true
+    }
+
+}
+
