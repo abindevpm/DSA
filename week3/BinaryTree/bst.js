4c315065aaf9395740f835c5b403e8fabd95085d
abindevpm <abindevpm6@gmail.com>
2026-09-04 12:26:22 +0530

    BST implemented

---
+++ week3/BinaryTree/bst.js
@@ -0,0 +1,64 @@
+class Node {
+    constructor(value){
+        this.value = value
+        this.left = null
+        this.right = null
+    }
+}
+
+class BST {
+    constructor(){
+        this.root = null
+    }
+
+     insert(value){
+        let newNode = new Node(value)
+         
+         if(this.root === null){
+            this.root = newNode
+         }else{
+            this.insertNode(this.root,newNode)
+         }
+     }
+
+     insertNode(node,newNode){
+        if(newNode.value<node.value){
+            if(node.left === null){
+                node.left = newNode
+            }else{
+                this.insertNode(node.left,newNode)
+            }
+        }else{
+            if(node.right === null){
+                node.right = newNode
+            }else{
+                this.insertNode(node.right,newNode)
+            }
+        }
+     }
+
+
+
+
+
+   inorder(node = this.root){
+    if(node === null){
+        return
+    }
+    this.inorder(node.left)
+    console.log(node.value)
+    this.inorder(node.right)
+   }
+
+}
+
+
+
+
+let tree = new BST()
+
+tree.insert(22)
+tree.insert(10)
+tree.insert(1)
+
+tree.inorder()
